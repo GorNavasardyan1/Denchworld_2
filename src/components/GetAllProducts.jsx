2899e9ecfed0e1ea768c660149a7676c4cdd73f1
GorNavasardyan1 <navasardyan_gor@internet.ru>
2023-10-28 18:07:30 +0400

    added basket shop

--- src/components/GetAllProducts.jsx
+++ src/components/GetAllProducts.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { AllProducts } from '../api'
 import { useNavigate } from 'react-router-dom'
 
-export default function GetAllProducts() {
+export default function GetAllProducts({add}) {
     const [allProducts,setAllProducts] = useState([])
     const navigate = useNavigate()
     useEffect(() => {
@@ -18,8 +18,7 @@ export default function GetAllProducts() {
             <h1 className=' text-center font-semibold text-[26px]'>Լավագույն ապրանքներ</h1>
             <div className=' grid sm:grid-cols-2 lg:grid-cols-4'>
                 {
-                    allProducts.map(el => <div key={el._id}
-                        
+                    allProducts.map(el => <div><div key={el._id}       
                     className=' flex justify-center items-center flex-col m-4 p-4 cursor-pointer duration-300 hover:shadow-lg'
                     onClick={() => {
                         localStorage.setItem('productID',el._id)
@@ -27,6 +26,10 @@ export default function GetAllProducts() {
                             <img src={el.photos[0]?.url} alt="" className='h-[230px] w-[230px]'/>
                             <div className='font-semibold'>{el.title}</div>
                             <div>{el.price} ֏</div>
+                            </div>
+                            <div className=' m-2 w-full flex justify-center' >
+                            <button className=' px-2 py-2  bg-[#0156FF] rounded-[10px] hover:bg-white border-2 duration-300 z-50 w-[120px]' onClick={() => add(el)}>Ավելացնել զամբյուղ</button>
+                            </div>
                     </div>)
                 }
             </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,10 +6,15 @@ import { GetAllCategories } from '../../api'
 import FindAllCategories from '../../components/FindAllCategories'
 import GetAllProducts from '../../components/GetAllProducts'
 import Carousel from '../../components/carusel/Carusel'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Basket from '../../components/basket/Basket'
 
 export default function Home() {
   const [categories,setCategories] = useState([])
   const [loading,setLoading] = useState(true)
+  const [addToBasket,setAddToBasket] = useState([])
+  const [showBasket,setShowBasket] = useState(false)
   useEffect(() => {
     GetAllCategories().then(data => {
       setCategories(data)
@@ -17,14 +22,56 @@ export default function Home() {
     })
   },[])
 
+
+
+
+  /////////////////// basket logic
+  const add = (product) => {
+    const index = addToBasket.findIndex(el => el._id == product._id)
+    if(index == -1) {
+      const newProduct = {
+        ...product,
+        quantity: 1
+      }
+      setAddToBasket([...addToBasket,newProduct])
+    } else {
+      const newProduct = addToBasket.map(el => {
+        if(el._id === product._id) el.quantity = el.quantity + 1
+        return el
+      })
+      setAddToBasket(newProduct)
+    }
+  }
+  const plus = (id) => {
+    const newProduct = addToBasket.map(el => {
+      if(el._id === id) el.quantity++
+      return el
+    })
+    setAddToBasket(newProduct)
+  }
+  const minus = (id) => {
+    const newProduct = addToBasket.map(el => {
+      if(el._id === id) el.quantity = el.quantity > 1 ? el.quantity -1 : 1
+      return el
+    })
+    setAddToBasket(newProduct)
+  }
+  const remove = (product) => {
+    const newProduct = addToBasket.filter(el => el._id !== product._id)
+    setAddToBasket(newProduct)
+  }
+
+
+
   return (
     <>
       {loading && <Loading/>}
       {!loading && <div>
-        <Header/>
+        <Header setShowBasket={setShowBasket} showBasket={showBasket} addToBasket={addToBasket}/>
+        {addToBasket.length == 0 && showBasket ? <p className=' absolute right-2'>Զամբյուղը դատարկ է</p> : showBasket && <Basket addToBasket={addToBasket} remove={remove} plus={plus} minus={minus}/>}
         <Carousel/>
         <FindAllCategories categories={categories}/>
-        <GetAllProducts/>
+        <GetAllProducts add={add}/>
       </div>}
     </>
   )
