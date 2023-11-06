cb24fd0ac69ad2c3ab7719b652ada0f47c7e6151
Ruben <amon007>
2023-11-06 20:56:58 +0300

    Update TextEditor.jsx

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import ReactQuill from "react-quill";
 import Quill from "quill";
 import "react-quill/dist/quill.snow.css";
+
+
 
 export default function TextEditor({ markdownText, onMarkdownChange }) {
   const handleChange = (value) => {
     onMarkdownChange(value);
   };
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ["bold", "italic", "underline", "strike"],
+          [{ script: 'super' }, { script: 'sub' }],
+          [{ color: [] }, { background: [] }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+          [{ direction: "rtl" }],
+          ["link"],
+          ["clean"],
+        ],
+      },
+    }),
+    []
+  );
+
   
 
-  const modules = {
-    toolbar: [  
-      [{ 'color': ["red","green","white","yellowgreen","gray","orange"] }],
-      [{ "font": [] }],
-      ['bold', 'italic', 'underline', 'strike'], 
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }], 
-      ['link'],
-      ['clean'], 
-    ],
-  };
-  
-
-  return <div className="w-[400px]">
-  <ReactQuill className="w-full h-[260px]" theme="snow"  modules={modules} value={markdownText} onChange={handleChange} />
-  </div>
+  return (
+    <div className="w-[400px]">
+      <ReactQuill
+        className="w-full h-[260px]"
+        theme="snow"
+        bounds={'.app'}
+        modules={modules}
+        value={markdownText}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
